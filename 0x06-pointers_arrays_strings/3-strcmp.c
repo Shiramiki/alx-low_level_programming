315541ac1f869d4022447d27a676d26c8f389144
Shiramiki <mbeizacokila@gmail.com>
2023-03-04 04:55:32 -0800

    Rachel's commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+  * _strcmp - compares two strings.
+  * @s1: string to compare
+  * @s2: string to compare
+  *
+  * Return: integer based on comparison
+*/
+int _strcmp(char *s1, char *s2)
+{}
